3028131fb0778ab0206bd689f0424460df8715f3
kd0098 <39730668+kd0098@users.noreply.github.com>
2019-01-01 02:07:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.abc = "abcd";
+    $scope.questionPaper = [
+    {
+    	question: " this is first question",
+    	option:["first", "second", "third", "forth"],
+    	answer: "first"
+    },
+    {
+    	question: " this is second question",
+    	option:["first", "second", "third", "forth"],
+    	answer: "second"
+    }
+    ];
+    $scope.answers = [];
+    $scope.score = 0;
+    $scope.debug = false;
+    
+    hideQuestion = false;
+    showScore = false;
+    
+    $scope.questionVisibility = function() {
+    	$scope.hideQuestion = !$scope.hideQuestion;
+    }
+
+    $scope.evaluate = function() {
+    	len = $scope.questionPaper.length;
+    	count = 0;
+    	console.log(len);
+    	for(i=0; i<len; ++i) {
+    		if($scope.questionPaper[i].answer === $scope.answers[i])
+    			++count;
+    	}
+    	$scope.score = count;
+    }
+
+    $scope.nextStep = function() {
+    	$scope.showScore = true;
+    	$scope.questionVisibility();
+    	$scope.evaluate();
+    }
 });
+
+
+
